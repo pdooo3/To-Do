ec5b6de455ae981ad8219df1d42b4bde4b57ea2b
mahdi <mahdi.ma.0919@gmail.com>
2024-10-04 14:26:25 +0330

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./style.css";
+const App = () => {
+  const [sec, setSec] = useState(0);
+  const [min, setMin] = useState(0);
+  const [state, setState] = useState("stop");
+  var timer;
+  var sOrS = true;
 
-function App() {
+  useEffect(() => {
+    if (sOrS) {
+      timer = setInterval(() => {
+        setSec(sec + 1);
+        if (sec === 59) {
+          setMin(min + 1);
+          setSec(0);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <progress value={sec} max={59}></progress>
+      <h1>
+        {min < 10 ? "0" + min : min}:{sec < 10 ? "0" + sec : sec}
+      </h1>
+      <button
+        onClick={() => {
+          setSec(0);
+          setMin(0);
+        }}
+      >
+        reset time
+      </button>
+      <button
+        onClick={() => {
+          if (sOrS) {
+            clearInterval(timer);
+            sOrS = false;
+          } else {
+            setSec(sec + 1);
+            if (sec == 59) {
+              setMin(min + 1);
+              setSec(0);
+            }
+          }
+        }}
+      >
+        stop start
+      </button>
+    </>
   );
-}
-
+};
 export default App;
